4d84f874b04e38456acddfbb872e3de184ad5a84
Nandani Tyagi <krishnatyagi450@gmail.com>
2026-07-30 20:26:13 +0530

    Add files via upload

---
+++ Main-1.java
@@ -0,0 +1,32 @@
+// Simple basic Calculation Calculator.
+
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int calculate = sc.nextInt();
+        switch (calculate) {
+            case 1:
+                System.out.println(a+b);
+                break;
+            case 2:
+                System.out.println(a-b);
+                break;
+            case 3:
+                System.out.println(a*b);
+                break;
+            case 4:
+                System.out.println(a/b);
+                break;
+            case 5:
+                System.out.println(a%b);
+                break;
+            
+            default:
+                System.out.println("invalid");
+        }
+    }
+}
